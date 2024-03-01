0b5ed17b6ac54f4b61e446589bf2d27bc73e4d87
ibenliben <iben@reiremo.no>
2024-03-01 11:34:07 +0100

    css og js

--- SoME-GDPR.js
+++ SoME-GDPR.js
@@ -6,49 +6,78 @@ let erFaceKLikket = false
 let erInstaKLikket = false
 let erTiktokKlikket = false
 
-function showFace() {
-    
-    if (erFaceKLikket == false) {
+let fbi = document.querySelector(".fa-brands.fa-facebook")
+let igi = document.querySelector(".fa-brands.fa-instagram")
+let tti = document.querySelector(".fa-brands.fa-tiktok")
 
+function showFace() {
+    if (erFaceKLikket == false) {
         console.log("face klikket")
-        erFaceKLikket = true 
+
+        erFaceKLikket = true
+        erInstaKLikket = false
+        erTiktokKlikket = false
+
         fb.style.display = "block"
         ig.style.display = "none"
         tt.style.display = "none"
 
-    } else {
+        fbi.classList.add("stor")
+        igi.classList.remove("stor")
+        tti.classList.remove("stor")
 
+    } else {
         fb.style.display = "none"
         erFaceKLikket = false
+
+        fbi.classList.remove("stor")
     }
 }
 
 function showInsta() {
     if (erInstaKLikket == false) {
-
         console.log("insta klikket")
+
         erInstaKLikket = true
+        erFaceKLikket = false
+        erTiktokKlikket = false
+
         ig.style.display = "block"
         fb.style.display = "none"
         tt.style.display = "none"
+
+        igi.classList.add("stor")
+        fbi.classList.remove("stor")
+        tti.classList.remove("stor")
+
     } else {
         ig.style.display = "none"
         erInstaKLikket = false
+
+        igi.classList.remove("stor")
     }
 }
 
 function showTiktok() {
     if (erTiktokKlikket == false) {
-
         console.log("tiktok klikket")
+
         erTiktokKlikket = true
+        erInstaKLikket = false
+        erFaceKLikket = false
+
         tt.style.display = "block"
         ig.style.display = "none"
         fb.style.display = "none"
-
+  
+        tti.classList.add("stor")    
+        igi.classList.remove("stor")
+        fbi.classList.remove("stor")
+  
     } else {
-        
         tt.style.display = "none"
         erTiktokKlikket = false
+
+        tti.classList.remove("stor")
     }
 }
