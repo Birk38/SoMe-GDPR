81b84f1c03de9c972941f50ea2786f620e085682
Lajorii <jonas.riiserlarsen@gmail.com>
2024-03-01 10:10:34 +0100

    Fikset display på apper i javascript

--- SoME-GDPR.js
+++ SoME-GDPR.js
@@ -2,32 +2,53 @@ let fb = document.getElementById("face")
 let ig = document.getElementById("insta")
 let tt = document.getElementById("tiktok")
 
+let erFaceKLikket = false
+let erInstaKLikket = false
+let erTiktokKlikket = false
+
 function showFace() {
-    if (fb.style.display == "none") {
-        fb.style.display == "block"
-        ig.style.display == "none"
-        tt.style.display == "none"
+    
+    if (erFaceKLikket == false) {
+
+        console.log("face klikket")
+        erFaceKLikket = true 
+        fb.style.display = "block"
+        ig.style.display = "none"
+        tt.style.display = "none"
+
     } else {
-        fb.style.display == "none"
+
+        fb.style.display = "none"
+        erFaceKLikket = false
     }
 }
 
 function showInsta() {
-    if (ig.style.display == "none") {
-        ig.style.display == "block"
-        fb.style.display == "none"
-        tt.style.display == "none"
+    if (erInstaKLikket == false) {
+
+        console.log("insta klikket")
+        erInstaKLikket = true
+        ig.style.display = "block"
+        fb.style.display = "none"
+        tt.style.display = "none"
     } else {
-        ig.style.display == "none"
+        ig.style.display = "none"
+        erInstaKLikket = false
     }
 }
 
 function showTiktok() {
-    if (tt.style.display == "none") {
-        tt.style.display == "block"
-        ig.style.display == "none"
-        fb.style.display == "none"
+    if (erTiktokKlikket == false) {
+
+        console.log("tiktok klikket")
+        erTiktokKlikket = true
+        tt.style.display = "block"
+        ig.style.display = "none"
+        fb.style.display = "none"
+
     } else {
-        tt.style.display == "none"
+        
+        tt.style.display = "none"
+        erTiktokKlikket = false
     }
 }
