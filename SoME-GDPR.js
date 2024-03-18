d6c3be94cf20c0df23bef9ae5f32b44068e67660
Birk38 <152275425+Birk38@users.noreply.github.com>
2024-03-18 09:01:18 +0100

    Formateringsfaen

--- SoME-GDPR.js
+++ SoME-GDPR.js
@@ -1,83 +1,80 @@
-let fb = document.getElementById("face") 
-let ig = document.getElementById("insta")
-let tt = document.getElementById("tiktok")
+let fb = document.getElementById("face");
+let ig = document.getElementById("insta");
+let tt = document.getElementById("tiktok");
 
-let erFaceKLikket = false
-let erInstaKLikket = false
-let erTiktokKlikket = false
+let erFaceKLikket = false;
+let erInstaKLikket = false;
+let erTiktokKlikket = false;
 
-let fbi = document.querySelector(".fa-brands.fa-facebook")
-let igi = document.querySelector(".fa-brands.fa-instagram")
-let tti = document.querySelector(".fa-brands.fa-tiktok")
+let fbi = document.querySelector(".fa-brands.fa-facebook");
+let igi = document.querySelector(".fa-brands.fa-instagram");
+let tti = document.querySelector(".fa-brands.fa-tiktok");
 
 function showFace() {
-    if (erFaceKLikket == false) {
-        console.log("face klikket")
+  if (erFaceKLikket == false) {
+    console.log("face klikket");
 
-        erFaceKLikket = true
-        erInstaKLikket = false
-        erTiktokKlikket = false
+    erFaceKLikket = true;
+    erInstaKLikket = false;
+    erTiktokKlikket = false;
 
-        fb.style.display = "block"
-        ig.style.display = "none"
-        tt.style.display = "none"
+    fb.style.display = "block";
+    ig.style.display = "none";
+    tt.style.display = "none";
 
-        fbi.classList.add("stor")
-        igi.classList.remove("stor")
-        tti.classList.remove("stor")
+    fbi.classList.add("stor");
+    igi.classList.remove("stor");
+    tti.classList.remove("stor");
+  } else {
+    fb.style.display = "none";
+    erFaceKLikket = false;
 
-    } else {
-        fb.style.display = "none"
-        erFaceKLikket = false
-
-        fbi.classList.remove("stor")
-    }
+    fbi.classList.remove("stor");
+  }
 }
 
 function showInsta() {
-    if (erInstaKLikket == false) {
-        console.log("insta klikket")
+  if (erInstaKLikket == false) {
+    console.log("insta klikket");
 
-        erInstaKLikket = true
-        erFaceKLikket = false
-        erTiktokKlikket = false
+    erInstaKLikket = true;
+    erFaceKLikket = false;
+    erTiktokKlikket = false;
 
-        ig.style.display = "block"
-        fb.style.display = "none"
-        tt.style.display = "none"
+    ig.style.display = "block";
+    fb.style.display = "none";
+    tt.style.display = "none";
 
-        igi.classList.add("stor")
-        fbi.classList.remove("stor")
-        tti.classList.remove("stor")
+    igi.classList.add("stor");
+    fbi.classList.remove("stor");
+    tti.classList.remove("stor");
+  } else {
+    ig.style.display = "none";
+    erInstaKLikket = false;
 
-    } else {
-        ig.style.display = "none"
-        erInstaKLikket = false
-
-        igi.classList.remove("stor")
-    }
+    igi.classList.remove("stor");
+  }
 }
 
 function showTiktok() {
-    if (erTiktokKlikket == false) {
-        console.log("tiktok klikket")
+  if (erTiktokKlikket == false) {
+    console.log("tiktok klikket");
 
-        erTiktokKlikket = true
-        erInstaKLikket = false
-        erFaceKLikket = false
+    erTiktokKlikket = true;
+    erInstaKLikket = false;
+    erFaceKLikket = false;
 
-        tt.style.display = "block"
-        ig.style.display = "none"
-        fb.style.display = "none"
-  
-        tti.classList.add("stor")    
-        igi.classList.remove("stor")
-        fbi.classList.remove("stor")
-  
-    } else {
-        tt.style.display = "none"
-        erTiktokKlikket = false
+    tt.style.display = "block";
+    ig.style.display = "none";
+    fb.style.display = "none";
 
-        tti.classList.remove("stor")
-    }
+    tti.classList.add("stor");
+    igi.classList.remove("stor");
+    fbi.classList.remove("stor");
+  } else {
+    tt.style.display = "none";
+    erTiktokKlikket = false;
+
+    tti.classList.remove("stor");
+  }
 }
--- meningsmåling.js
+++ meningsmåling.js
@@ -1,102 +1,67 @@
-let jaButton = document.getElementById("jaButton")
-let neiButton = document.getElementById("neiButton")
+let jaButton = document.getElementById("jaButton");
+let neiButton = document.getElementById("neiButton");
 
-jaButton.onclick = sendJa
-neiButton.onclick = sendNei
+jaButton.onclick = sendJa;
+neiButton.onclick = sendNei;
 
-const URL = "https://rasmusweb.no/spm.php"
-const sporsmolsId = "undersokelse2"
+const URL = "https://rasmusweb.no/spm.php";
+const sporsmolsId = "undersokelse2";
 
-  async function sendJa() {
-    const response = await fetch(URL, {
-        method: 'POST',
-        headers: {
-       //   'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({id: sporsmolsId, svar: "Ja" })
-      })
-      console.log("gikk det bra?", response.ok)
-      getRequest();
+async function sendJa() {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {},
+    body: JSON.stringify({ id: sporsmolsId, svar: "Ja" }),
+  });
+  console.log("gikk det bra?", response.ok);
+  getRequest();
 }
 
 async function sendNei() {
-    const response = await fetch(URL, {
-        method: 'POST',
-        body: JSON.stringify({id: sporsmolsId, svar: "Nei" })
-      })
-      getRequest();
+  const response = await fetch(URL, {
+    method: "POST",
+    body: JSON.stringify({ id: sporsmolsId, svar: "Nei" }),
+  });
+  getRequest();
 }
 
-getRequest()
+getRequest();
 setInterval(getRequest, 60000);
 
 async function getRequest() {
-  const htmlObj = document.getElementById("getResult")
-  htmlObj.innerHTML = "Waiting for response"
+  const htmlObj = document.getElementById("getResult");
+  htmlObj.innerHTML = "Waiting for response";
 
   const apiCallPromise = await fetch(URL + "?id=" + sporsmolsId, {
     method: "GET",
-    //  mode: "no-cors", // no-cors, *cors, same-origin
     headers: {
       Accept: "application/json",
     },
-    // Kan ikke ha med body i GET request. body: JSON.stringify({ id: "forEu" }),
-  })
+  });
 
-  htmlObj.innerHTML = ""
+  htmlObj.innerHTML = "";
 
-/* const p = document.createElement("p")
-   p.textContent = "StatusCodeOK: " + apiCallPromise.ok
-   htmlObj.appendChild(p) */
+  const entries = await apiCallPromise.json();
+  console.log(entries);
+  console.log(entries.length);
 
-  // Getting the json entries from the response:
-  const entries = await apiCallPromise.json()
-  console.log(entries)
-  console.log(entries.length)
-
-   let data = [
+  let data = [
     {
       x: [entries[1].antall, entries[0].antall],
-      y: [entries[1].svar,entries[0].svar],
-      type: 'bar',
-      orientation: 'h',
+      y: [entries[1].svar, entries[0].svar],
+      type: "bar",
+      orientation: "h",
       marker: {
-        color: ['#2A2D43','#2A2D43']
-    }}
-  ]
+        color: ["#2A2D43", "#2A2D43"],
+      },
+    },
+  ];
 
   let layout = {
     yaxis: {
-      showticklabels: false
-    }
-  }
+      showticklabels: false,
+    },
+  };
 
-  Plotly.newPlot('soylediagram', data, layout,{displayModeBar: false})
+  Plotly.newPlot("soylediagram", data, layout, { displayModeBar: false });
 }
-
-
-
-
-  // for (let i = 0; i <= entries.length; i++) {
-  //   let data = [
-  //     {
-  //       x: [entries[i].svar],
-  //       y: [entries[i].antall],
-  //       type: 'bar'
-  //     }
-  //   ]
-  //   Plotly.newPlot('soylediagram', data)
-  // }
-
-//function JSONtoSTR(resultJson){
-//  const obj = JSON.parse(resultJson) 
-//  console.log(obj[1]);
-//  const output_nei = obj[1];
-//  const { svar: svar_nei, antall: antall_nei } = output_nei;
-//  const nei = antall_ne 
-//  console.log(obj[0]);
-//  const output_ja = obj[0];
-//  const { svar: svar_ja, antall: antall_ja } = output_ja;
-//  const ja = antall_j 
-//  console.log("nei: " + nei, "ja: " + ja);
-//}
